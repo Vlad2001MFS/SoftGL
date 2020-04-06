a1f9db7ee7f0245674e634f67107cd033fdb3302
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-04-06 15:05:11 +0300

    add non-simd triangle rasterization

--- Rasterizer.cpp
+++ Rasterizer.cpp
@@ -67,8 +67,65 @@ __forceinline bool __fastcall compareFunc(uint32_t func, T lhs, T rhs) {
     }
 }
 
-void drawTriangle(const Vec2i &bufferSize, bool isDepthTest, uint32_t depthFunc, const Vec2i &vpMin, const Vec2i &vpMax,
-                  const Vertex &A, const Vertex &B, const Vertex &C) {
+void drawTriangleBarycentric(const Vec2i &bufferSize, bool isDepthTest, uint32_t depthFunc, const Vec2i &vpMin, const Vec2i &vpMax,
+                             const Vertex &A, const Vertex &B, const Vertex &C) {
+    const Vec4f colorBCA[] = {
+        Vec4f(B.color.r, C.color.r, A.color.r, 0.0f),
+        Vec4f(B.color.g, C.color.g, A.color.g, 0.0f),
+        Vec4f(B.color.b, C.color.b, A.color.b, 0.0f),
+        Vec4f(B.color.a, C.color.a, A.color.a, 0.0f),
+    };
+
+    const auto triMin = Vec2i(Vec4f::min(A.pos, B.pos, C.pos));
+    const auto triMax = Vec2i(Vec4f::max(A.pos, B.pos, C.pos));
+    const auto min = Vec2i::clamp(triMin, vpMin, vpMax);
+    const auto max = Vec2i::clamp(triMax, vpMin, vpMax);
+
+    const auto invABCz = Vec4f(1.0f / A.pos.z, 1.0f / B.pos.z, 1.0f / C.pos.z, 0.0f);
+
+    const Vec4f AC = Vec4f(C.pos - A.pos);
+    const Vec4f AB = Vec4f(B.pos - A.pos);
+
+    const float bcW = AB.x*AC.y - AC.x*AB.y;
+    const float bcInvW = 1.0f / bcW;
+
+    for (int y = min.y; y <= max.y; y++) {
+        const int PAy = A.pos.y - y;
+
+        for (int x = min.x; x <= max.x; x++) {
+            const int PAx = A.pos.x - x;
+
+            const float bcU = AC.x*PAy - PAx*AC.y;
+            const float bcV = PAx*AB.y - AB.x*PAy;
+
+            auto bcScreen = Vec4f(bcU, bcV, 0.0f, 0.0f);
+            bcScreen *= bcInvW;
+            bcScreen.z = 1.0f - bcScreen.x - bcScreen.y;
+
+            if (bcScreen.x >= 0 && bcScreen.y >= 0 && bcScreen.z >= 0) {
+                Vec4f bcClip = bcScreen*invABCz;
+                bcClip /= bcClip.x + bcClip.y + bcClip.z; // perspective-correction
+
+                const uint32_t idx = x + y*bufferSize.x;
+                if (isDepthTest) {
+                    const float depth = bcClip.x*B.pos.z + bcClip.y*C.pos.z + bcClip.z*A.pos.z;
+                    if (!compareFunc(depthFunc, depth, gDepthBuffer[idx])) {
+                        continue;
+                    }
+                    gDepthBuffer[idx] = depth;
+                }
+
+                gColorBuffer[idx][0] = bcScreen.dot(colorBCA[0]);
+                gColorBuffer[idx][1] = bcScreen.dot(colorBCA[1]);
+                gColorBuffer[idx][2] = bcScreen.dot(colorBCA[2]);
+                gColorBuffer[idx][3] = bcScreen.dot(colorBCA[3]);
+            }
+        }
+    }
+}
+
+void drawTriangleBarycentricSIMD(const Vec2i &bufferSize, bool isDepthTest, uint32_t depthFunc, const Vec2i &vpMin, const Vec2i &vpMax,
+                                 const Vertex &A, const Vertex &B, const Vertex &C) {
     __m128 BCAr_SIMD = _mm_setr_ps(B.color.r, C.color.r, A.color.r, 0.0f);
     __m128 BCAg_SIMD = _mm_setr_ps(B.color.g, C.color.g, A.color.g, 0.0f);
     __m128 BCAb_SIMD = _mm_setr_ps(B.color.b, C.color.b, A.color.b, 0.0f);
@@ -155,7 +212,7 @@ void processTriangles() {
         const Vertex &B = verts[i + 1];
         const Vertex &C = verts[i + 2];
 
-        drawTriangle(bufferSize, isDepthTest, depthFunc, vpMin, vpMax, A, B, C);
+        drawTriangleBarycentricSIMD(bufferSize, isDepthTest, depthFunc, vpMin, vpMax, A, B, C);
     }
 }
 
